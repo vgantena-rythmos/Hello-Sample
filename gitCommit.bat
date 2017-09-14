99af82387ed0588c980019870b64550f4f1a52a4
Venkateswarlu Gantena <vgantena@rythmos.com>
2017-09-14 15:32:02 +0530

    made changes with new version...!

--- gitCommit.bat
+++ gitCommit.bat
@@ -3,7 +3,7 @@ set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set path=C:\Program Files\Git\bin
 SET CurrentDir=%~dp0
 git add .
-git commit -am "made changes"
+git commit -am "made changes with new version...!"
 git push
 echo Press Enter...
 pause
